7587a05f0b6e80bf16f1ae395f07af2278986aa2
unknown <Ralph Angelo Badang>
2024-09-19 11:13:13 +0800

    added the heron's formula and the formula for the leap year

---
+++ PracticeSets03Solutions/ConsoleApplication1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+        static double HeronsFormula(double sideA, double sideB, double sideC)
+        {
+            double s = (sideA + sideB + sideC) / 2;
+
+            double Area = Math.Sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
+
+            return Area;
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Triangle side A: ");
+            double A = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the Triangle side B: ");
+            double B = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the Triangle side C: ");
+            double C = Convert.ToDouble(Console.ReadLine());
+
+            double result = HeronsFormula(A, B, C);
+
+            Console.WriteLine("The Area of the Triangle using the Heron's Formula is: " + result);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ PracticeSets03Solutions/ConsoleApplication2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+
+        static bool leapYear(double ly)
+        {
+            if (ly % 4 == 0 && (ly % 100 != 0 || ly % 400 == 0)){
+                return true;
+            }
+
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a year: ");
+            double userinput = Convert.ToDouble(Console.ReadLine());
+
+            bool result = leapYear(userinput);
+            
+            if (result == true) {
+            Console.WriteLine("The year " + userinput + " is a leap year");
+                }
+            else
+            {
+                Console.WriteLine("The year " + userinput + " is not a leap year");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
